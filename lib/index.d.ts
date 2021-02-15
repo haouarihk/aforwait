b0349c20e126f3f516059d253950ca83c43dbfd7
wolfpat01 <haithem2001@gmail.com>
2021-02-15 23:56:02 +0100

    first commit

---
+++ lib/index.d.ts
@@ -0,0 +1,40 @@
+/**
+ *
+ * @param time The Waiting time in ms.
+ */
+export default function afor(time: number): Promise<void>;
+/**
+ *
+ * @param time The Waiting time in Minutes.
+ */
+export declare function aforSec(time: number): Promise<void>;
+/**
+ *
+ * @param time The Waiting time in Minutes.
+ */
+export declare function aforMin(time: number): Promise<void>;
+/**
+ *
+ * @param time The Waiting time in Hours.
+ */
+export declare function aforHours(time: number): Promise<void>;
+/**
+ *
+ * @param time The Waiting time in Days.
+ */
+export declare function aforDays(time: number): Promise<void>;
+/**
+ *
+ * @param time The Waiting time in Years.
+ */
+export declare function aforYears(time: number): Promise<void>;
+/**
+ *
+ * @param time The Waiting time in Decades.
+ */
+export declare function aforDecades(time: number): Promise<void>;
+/**
+ *
+ * @param time The Waiting time in Infinite.
+ */
+export declare function aforInfinit(time: number): Promise<void>;
