7c9b03d05dc43274f26210b694334a001dd0094b
wolfpat01 <haithem2001@gmail.com>
2021-02-16 12:04:24 +0100

    fixing configurqtions

--- lib/test.d.ts
+++ lib/test.d.ts
@@ -1,1 +1,2 @@
-export {};
+declare const aforwait: any;
+declare const aforSec: any, aforMin: any, aforInfinit: any, aforDays: any, aforHours: any, aforYears: any, aforDecades: any;
--- test.js
+++ test.js
@@ -1,8 +1,17 @@
-import *  as aforwait from "./lib/index.js";
+const aforwait = require("./lib/index.js");
 
+const {
+    aforSec,
+    aforMin,
+    aforInfinit,
+    aforDays,
+    aforHours,
+    aforYears,
+    aforDecades
+} = aforwait;
 
 (async () => {
     console.log("i'm gonna wait 5 seconds and then tell you yee")
-    await aforwait.aforSec(5) // await aforSec(5);
+    await aforSec(5) // await aforSec(5);
     console.log("yee")
 })()
